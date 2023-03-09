28abbaeebcf363197f114c3ba3e912b11ca98df2
davay <davay@defucc.me>
2023-03-09 17:29:20 +0700

    feat: ES6 version

---
+++ src/es6/amy.worklet.js
@@ -0,0 +1,71 @@
+/**
+  An in-place replacement for ScriptProcessorNode using AudioWorklet
+  //TODO Now it's just an example from the link below:
+  - https://gist.github.com/louisgv/f210a1139d955baf511ff35f58fc8db1
+  - https://github.com/Tonejs/Tone.js/issues/712
+*/
+class AmyProcessor extends AudioWorkletProcessor {
+  // 0. Determine the buffer size (this is the same as the 1st argument of ScriptProcessor)
+  bufferSize = 4096
+  // 1. Track the current buffer fill level
+  _bytesWritten = 0
+
+  // 2. Create a buffer of fixed size
+  _buffer = new Float32Array(this.bufferSize)
+
+  constructor() {
+    this.initBuffer()
+  }
+
+  initBuffer() {
+    this._bytesWritten = 0
+  }
+
+  isBufferEmpty() {
+    return this._bytesWritten === 0
+  }
+
+  isBufferFull() {
+    return this._bytesWritten === this.bufferSize
+  }
+
+  /**
+ * @param {Float32Array[][]} inputs
+ * @returns {boolean}
+ */
+  process(inputs) {
+    // Grabbing the 1st channel similar to ScriptProcessorNode
+    this.append(inputs[0][0])
+
+    return true
+  }
+
+  /**
+   *
+   * @param {Float32Array} channelData
+   */
+  append(channelData) {
+    if (this.isBufferFull()) {
+      this.flush()
+    }
+
+    if (!channelData) return
+
+    for (let i = 0; i < channelData.length; i++) {
+      this._buffer[this._bytesWritten++] = channelData[i]
+    }
+  }
+
+  flush() {
+    // trim the buffer if ended prematurely
+    this.port.postMessage(
+      this._bytesWritten < this.bufferSize
+        ? this._buffer.slice(0, this._bytesWritten)
+        : this._buffer
+    )
+    this.initBuffer()
+  }
+
+}
+
+registerProcessor("amy.worklet", AmyProcessor)
