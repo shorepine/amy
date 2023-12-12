a84f9fff7a595668d5e7ee3642e6e71884f24711
Brian Whitman <brian@variogr.am>
2023-12-11 19:59:49 -0800

    stubs for Arduino

---
+++ src/AMY-Arduino.cpp
@@ -0,0 +1,24 @@
+// AMY-Arduino.cpp
+// connector for Arduino
+
+#include "Arduino.h"
+#include "AMY-Arduino.h"
+
+// TODO -- see how other synth libraries deal with different audio output & pins, I2S, DAC, analog, etc
+AMY::AMY()
+{
+
+}
+
+void AMY::begin()
+{
+
+}
+
+void AMY::send_message(char * message) {
+
+}
+
+uint16_t * AMY::render_block() {
+
+}
---
+++ src/AMY-Arduino.h
@@ -0,0 +1,21 @@
+// AMY-Arduino.h
+// connector for Arduino
+
+#ifndef AMYARDUINOH
+#define AMYARDUINOH
+
+#include "Arduino.h"
+#include "amy.h"
+
+class AMY
+{
+  public:
+    AMY();
+    void begin();
+    void AMY::send_message(char * message);
+    uint16_t * render_block();
+  private:
+    int _pin;
+};
+
+#endif
