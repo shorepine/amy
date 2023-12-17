e4b1b792f841bf8f33bf135b15d6b7164f9488b6
Dan Ellis <dan.ellis@gmail.com>
2023-12-16 22:43:12 -0500

    amy-example -o file.wav writes to a wav file, for debug.

--- src/amy-example.c
+++ src/amy-example.c
@@ -5,17 +5,22 @@
 
 #include "amy.h"
 #include "examples.h"
+#include "miniaudio.h"
 #include "libminiaudio-audio.h"
 
 int main(int argc, char ** argv) {
+    char output_filename = NULL;
 
     int opt;
-    while((opt = getopt(argc, argv, ":d:lh")) != -1) 
+    while((opt = getopt(argc, argv, ":d:o:lh")) != -1) 
     { 
         switch(opt) 
         { 
             case 'd': 
                 amy_device_id = atoi(optarg);
+                break;
+            case 'o':
+                output_filename = strdup(optarg);
                 break;
             case 'l':
                 amy_print_devices();
@@ -36,9 +41,22 @@ int main(int argc, char ** argv) {
                 break; 
         } 
     }
-    uint32_t start =amy_sysclock();
+    uint32_t start = amy_sysclock();
     amy_start();
-    amy_live_start();
+
+    ma_encoder_config config = ma_encoder_config_init(ma_encoding_format_wav, ma_format_s16, AMY_NCHANS, AMY_SAMPLE_RATE);
+    ma_encoder encoder;
+    ma_result result;
+    if (output_filename) {
+        result = ma_encoder_init_file(output_filename, &config, &encoder);
+        if (result != MA_SUCCESS) {
+            fprintf(stderr, "Error : could not open file : %s (%d)\n", output_filename, result) ;
+            exit (1) ;
+        }
+    } else {
+        amy_live_start();
+    }
+
     amy_reset_oscs();
 
     //example_reverb();
@@ -47,13 +65,22 @@ int main(int argc, char ** argv) {
     //example_drums(start, 4);
     example_multimbral_fm(start);
 
-    // Now just spin for 20s
+    // Now just spin for 10s
     while(amy_sysclock() - start < 10000) {
+        if (output_filename) {
+            int16_t *frames = fill_audio_buffer_task();
+            int num_frames = AMY_BLOCK_SIZE;
+            result = ma_encoder_write_pcm_frames(&encoder, frames, num_frames, NULL);
+        }
         usleep(THREAD_USLEEP);
     }
     show_debug(3);
-    
-    amy_live_stop();
+
+    if (output_filename) {
+        ma_encoder_uninit(&encoder);
+    } else {
+        amy_live_stop();
+    }
 
     return 0;
 }
