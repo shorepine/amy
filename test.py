9c7cb43a8aacb3d6542ed54ab0a80baa68cfa4b3
Dan Ellis <dpwe@ee.columbia.edu>
2023-12-24 20:39:49 -0500

    exp2_lut/log2_lut used for default envelope.

--- test.py
+++ test.py
@@ -148,7 +148,7 @@ class TestFilter(AmyTest):
 
   def run(self):
     amy.send(time=0, osc=0, wave=amy.SAW_DOWN, filter_type=amy.FILTER_LPF, resonance=8.0, bp0_target=amy.TARGET_FILTER_FREQ, filter_freq=5000, bp0='0,1,500,0.01,100,0.0')
-    amy.send(time=100, note=48, vel=1.0)
+    amy.send(time=100, note=48, vel=1)
     amy.send(time=500, vel=0)
 
 
