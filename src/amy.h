39f08cf735ed47ace80962032a82cf40cdaba83d
Brian Whitman <brian@variogr.am>
2024-01-14 13:20:43 -0800

    div/0

--- src/amy.h
+++ src/amy.h
@@ -181,14 +181,14 @@ extern uint64_t profile_start_us;
     profiles[tag].calls++;
 
 #define AMY_PROFILE_PRINT(tag) \
-    fprintf(stderr,"%40s: %10"PRIu32" calls %10"PRIu64"us total [%6.2f%% wall %6.2f%% render] %9"PRIu64"us per call\n", \
-    profile_tag_name(tag), profiles[tag].calls, profiles[tag].us_total, \
-    ((float)(profiles[tag].us_total) / (float)(amy_get_us() - profile_start_us))*100.0, \
-    ((float)(profiles[tag].us_total) / (float)(profiles[AMY_RENDER].us_total))*100.0, \
-    (profiles[tag].us_total)/profiles[tag].calls);
-
+    if(profiles[tag].calls) {\
+        fprintf(stderr,"%40s: %10"PRIu32" calls %10"PRIu64"us total [%6.2f%% wall %6.2f%% render] %9"PRIu64"us per call\n", \
+        profile_tag_name(tag), profiles[tag].calls, profiles[tag].us_total, \
+        ((float)(profiles[tag].us_total) / (float)(amy_get_us() - profile_start_us))*100.0, \
+        ((float)(profiles[tag].us_total) / (float)(profiles[AMY_RENDER].us_total))*100.0, \
+        (profiles[tag].us_total)/profiles[tag].calls);\
+    }
 #else
-
 #define AMY_PROFILE_START(tag) void;
 #define AMY_PROFILE_STOP(tag) void;
 
