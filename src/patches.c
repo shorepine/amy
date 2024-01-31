fd2ea1f5ba20a3d2fc75be2e6e351e991ccbb625
Brian Whitman <brian@variogr.am>
2024-01-30 20:00:07 -0800

    adding patches.c

---
+++ src/patches.c
@@ -0,0 +1,28 @@
+// patches.c
+// baked in AMY string patches (Juno-6 for now)
+
+#include "amy.h"
+#include "juno.h"
+#define JUNO_PATCHES 128
+#define TOTAL_PATCHES 128
+
+void patches_init() {
+
+}
+
+void patches_setup_patch(uint16_t osc) {
+    char * message = juno_patch_commands[synth[osc].patch];
+    amy_play_message(message);
+}
+
+void patches_note_on(uint16_t osc) {
+     if(AMY_IS_SET(synth[osc].patch)) { 
+        patches_setup_patch(osc);
+    }
+}
+
+
+void patches_note_off(uint16_t osc) {
+    // Do whatever note_off does in juno.py
+}
+
