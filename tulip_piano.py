f6c72e6b781e5ce6c3c0093ff448435cd17bf745
Dan Ellis <dpwe@ee.columbia.edu>
2024-09-27 22:02:12 -0400

    tulip_piano: Add harmonic stretching, make PCM optional.

--- tulip_piano.py
+++ tulip_piano.py
@@ -18,15 +18,18 @@ with open(params_file, 'r') as f:
 
 pcm_patch = tulip.load_sample(residu_file)
 
-def setup_piano(bp_dict, pcm_patch, voices=None, base_freq=261.63):
+def setup_piano(bp_dict, pcm_patch=None, voices=None, base_osc=0, base_freq=261.63, stretch_coef=0.038):
   kwargs = {}
   if voices is not None:
     kwargs = {'voices': voices}
   num_partials = len(bp_dict)
-  base_osc = 0
   amy.send(osc=base_osc, wave=amy.BYO_PARTIALS, num_partials=num_partials, **kwargs)
 
-  for i in range(1, num_partials + 1):
+  harm_nums = range(1, num_partials + 1)
+  # Quadratic partial stretching
+  harm_freqs = [n * (261.8 + stretch_coef * n * n) for n in harm_nums]
+  
+  for i in harm_nums:
     # Set up each partial as the corresponding harmonic of the base_freq, with an amplitude of 1/N, 50ms attack, and a decay of 1 sec / N
     bp_vals = bp_dict[str(i)]
     # Predelay in ms
@@ -34,17 +37,20 @@ def setup_piano(bp_dict, pcm_patch, voices=None, base_freq=261.63):
     bp_string = ','.join("%d,%.3f" % (n, val) for n, val in bp_vals)
     bp_string += ',200,0'
     #print(bp_string)
-    amy.send(osc=base_osc + i, wave=amy.PARTIAL, freq=base_freq * i, bp0=bp_string, eg0_type=amy.ENVELOPE_TRUE_EXPONENTIAL, **kwargs)
+    amy.send(osc=base_osc + i, wave=amy.PARTIAL, freq=harm_freqs[i - 1], bp0=bp_string, eg0_type=amy.ENVELOPE_TRUE_EXPONENTIAL, **kwargs)
 
-  pcm_osc = num_partials + 1
-  #pcm_patch = tulip.load_sample(residu_file)
-  patch_note = 60 - 6  # Empirically
-  amy.send(osc=pcm_osc, wave=amy.CUSTOM, patch=pcm_patch, amp=10, **kwargs)
-  amy.send(osc=base_osc, chained_osc=pcm_osc, **kwargs)
+  if pcm_patch is not none:
+    pcm_osc = num_partials + 1
+    #pcm_patch = tulip.load_sample(residu_file)
+    patch_note = 60 - 6  # Empirically
+    amy.send(osc=pcm_osc, wave=amy.CUSTOM, patch=pcm_patch, amp=10, **kwargs)
+    amy.send(osc=base_osc, chained_osc=pcm_osc, **kwargs)
+
 
 def piano_note_on(note, vel=1):
   #amy.send(osc=pcm_osc, note=patch_note + (note - 60), vel=2*vel)
   amy.send(osc=0, note=note, vel=vel)
+
 
 #piano_note_on(60, 1)
 #time.sleep(1.0)
@@ -60,7 +66,7 @@ synth_obj = midi.Synth(num_voices=4, patch_string=patch_string)
 #voices = '0,1,2,3'
 voices = ",".join([str(a) for a in synth_obj.amy_voice_nums])
 
-print(voices)
+print("voices=", voices)
 
 setup_piano(bp_dict, pcm_patch, voices=voices)
 
