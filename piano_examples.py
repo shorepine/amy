964b3893c606d9c006f61cc9992d794c1368fe62
Dan Ellis <dan.ellis@gmail.com>
2024-12-30 00:00:35 -0500

    Remove cruft from piano_examples.py

--- piano_examples.py
+++ piano_examples.py
@@ -11,9 +11,6 @@ from experiments import tulip_piano
 import scipy.io.wavfile as wav
 
 
-
-
-
 def wavwrite(filename, data, samplerate=44100, force_mono=True):
   """Write a waveform to a WAV file."""
   if force_mono and len(data.shape) > 1:
@@ -21,41 +18,7 @@ def wavwrite(filename, data, samplerate=44100, force_mono=True):
   wav.write(filename, samplerate, (32768.0 * data).astype(np.int16))
 
 
-def init_piano_voices():
-    amy.send(store_patch='1024,' + tulip_piano.patch_string)
-    amy.send(time=0, voices='0,1,2', load_patch=1024)
-    for voice in ['0', '1', '2']:
-      tulip_piano.init_piano_voice(tulip_piano.num_partials, voices=voice, time=0)
-
-
-def tulip_piano_example(filename='piano_example_additive.wav', note_offset=-12):
-    # Allocate voices
-    amy.restart()
-    amy.send(time=0, volume=5)
-
-    init_piano_voices()
-
-    base_note = 74 + note_offset
-      
-    tulip_piano.piano_note_on(voices='0', vel=0.4, note=base_note, time=50)
-    tulip_piano.piano_note_on(voices='0', vel=0, time=440)
-    tulip_piano.piano_note_on(voices='0', vel=0.7, note=base_note, time=450)
-    tulip_piano.piano_note_on(voices='0', vel=0, time=840)
-    tulip_piano.piano_note_on(voices='0', vel=1.0, note=base_note, time=850)
-    tulip_piano.piano_note_on(voices='0', vel=0, time=1490)
-    tulip_piano.piano_note_on(voices='1', vel=0.6, note=base_note - 24, time=1500)
-    tulip_piano.piano_note_on(voices='2', vel=1.0, note=base_note + 24, time=2100)
-    tulip_piano.piano_note_on(voices='1', vel=0, time=3000)
-    tulip_piano.piano_note_on(voices='2', vel=0, time=3000)
-
-    samples = amy.render(3.3)
-
-    #amy.write(samples, filename)
-    wavwrite(filename, samples)
-    print('Wrote', len(samples), 'samples to', filename)
-
-
-def piano_example(patch_num=7, base_note=72, filename='piano_examples.wav', volume=5,
+def piano_example(base_note=72, filename='piano_examples.wav', volume=5,
                   send_command=amy.send, init_command=lambda: None):
     amy.restart()
     amy.send(time=0, volume=volume)
