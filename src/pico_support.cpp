b19071c29212ef6ef3e778b197344b14c8957d77
Dan Ellis <dpwe@ee.columbia.edu>
2025-07-04 12:05:45 -0400

    Working with PCM1808 audio_in (MCLK) on RP2350 - only 1 core, doesn't use TinyUSB.

--- src/pico_support.cpp
+++ src/pico_support.cpp
@@ -3,8 +3,9 @@
 //#include <Arduino.h>
 //#include <Adafruit_TinyUSB.h>
 //#include <MIDI.h>
+#ifdef USE_TUSB
 #include <Adafruit_TinyUSB.h>
-
+#endif
 
 
 
@@ -12,20 +13,24 @@ extern "C" {
 	void check_tusb_midi();
 	
 	void pico_setup_midi() {
-		if (!TinyUSBDevice.isInitialized()) {
+#ifdef USE_TUSB
+            if (!TinyUSBDevice.isInitialized()) {
 			TinyUSBDevice.begin(0);
 		}
 		//usb_midi.setStringDescriptor("AMY Synthesizer");
 
+#endif
 	}
 
 	void pico_process_midi() {
-		#ifdef TINYUSB_NEED_POLLING_TASK
+
+#ifdef USE_TUSB
+#ifdef TINYUSB_NEED_POLLING_TASK
 			// Manual call tud_task since it isn't called by Core's background
 			TinyUSBDevice.task();
 		#endif
 		check_tusb_midi();
+#endif
 	}
-
 }
 #endif
