01bf457317a883cf472b93dfe1e9538923c69b7d
Dan Ellis <dpwe@ee.columbia.edu>
2026-01-28 09:02:22 -0500

    Add example of retrieving events for user-defined patch to amy-example.c.

--- src/amy-example.c
+++ src/amy-example.c
@@ -22,6 +22,17 @@ uint8_t render(uint16_t osc, SAMPLE * buf, uint16_t len) {
 extern void *event_generator_for_patch_number(uint16_t patch_number, struct amy_event *event, void *state);
 extern void print_event(amy_event *e);
 
+void print_events_for_patch_number(int patch_number) {
+    void *state = NULL;
+    fprintf(stderr, "start delta_num_free = %d\n", delta_num_free());
+    do {
+        amy_event event = amy_default_event();
+        state = event_generator_for_patch_number(patch_number, &event, state);
+        print_event(&event);
+    } while (state != NULL);
+    fprintf(stderr, "end delta_num_free = %d\n", delta_num_free());
+}
+
 int main(int argc, char ** argv) {
     int8_t playback_device_id = -1;
     int8_t capture_device_id = -1;
@@ -68,17 +79,7 @@ int main(int argc, char ** argv) {
 
     for (int tries = 0; tries < 2; ++tries) {
     amy_start(amy_config);
-    
-    int patch_number = 0;
-    void *state = NULL;
-    fprintf(stderr, "start delta_num_free = %d\n", delta_num_free());
-    do {
-        amy_event event = amy_default_event();
-        state = event_generator_for_patch_number(patch_number, &event, state);
-        print_event(&event);
-    } while (state != NULL);
-    fprintf(stderr, "end delta_num_free = %d\n", delta_num_free());
-
+        
     //example_fm(0);
     //example_voice_chord(0,0);
     example_synth_chord(0, /* patch */ 0);
@@ -93,6 +94,77 @@ int main(int argc, char ** argv) {
     e.wave = SINE;
     amy_add_event(&e);
 
+    // Reading back a stored patch
+    int patch_number = 0;
+    print_events_for_patch_number(patch_number);
+    // Reading back a user-defined patch
+    patch_number = 1024;
+    e = amy_default_event();
+    e.patch_number = patch_number;
+    e.osc = 0;
+    e.wave = PULSE;
+    e.mod_source = 2;
+    e.amp_coefs[COEF_VEL] = 1.0f;
+    e.amp_coefs[COEF_EG0] = 1.0f;
+    e.amp_coefs[COEF_EG1] = 0;
+    e.freq_coefs[COEF_CONST] = 130.81f;
+    e.freq_coefs[COEF_NOTE] = 1.f;
+    e.portamento_ms = 0;
+    strcpy(e.bp0, "30,1,1355,0.354,232,0");
+    amy_add_event(&e);
+
+    e = amy_default_event();
+    e.patch_number = patch_number;
+    e.osc = 0;
+    e.chained_osc = 1;
+    e.filter_type = FILTER_LPF24;
+    e.filter_freq_coefs[COEF_CONST] = 126.54f;
+    e.filter_freq_coefs[COEF_NOTE] = 0.677f;
+    e.filter_freq_coefs[COEF_EG1] = 5.024f;
+    e.resonance = 0.93f;
+    strcpy(e.bp1, "30,1,1355,0.354,232,0");
+    amy_add_event(&e);
+
+    e = amy_default_event();
+    e.patch_number = patch_number;
+    e.osc = 1;
+    e.wave = SAW_UP;
+    e.mod_source = 2;
+    e.amp_coefs[COEF_VEL] = 1.0f;
+    e.amp_coefs[COEF_EG0] = 1.0f;
+    e.amp_coefs[COEF_EG1] = 0;
+    e.freq_coefs[COEF_CONST] = 130.81f;
+    e.freq_coefs[COEF_NOTE] = 1.f;
+    e.portamento_ms = 0;
+    strcpy(e.bp0, "30,1,1355,0.354,232,0");
+    amy_add_event(&e);
+
+    e = amy_default_event();
+    e.patch_number = patch_number;
+    e.osc = 2;
+    e.wave = TRIANGLE;
+    e.mod_source = 2;
+    e.amp_coefs[COEF_CONST] = 1.f;
+    e.amp_coefs[COEF_VEL] = 0;
+    e.amp_coefs[COEF_EG0] = 1.0f;
+    e.freq_coefs[COEF_CONST] = 4.0f;
+    e.freq_coefs[COEF_NOTE] = 0;
+    strcpy(e.bp0, "0,1,10000,0");
+    amy_add_event(&e);
+
+    e = amy_default_event();
+    e.patch_number = patch_number;
+    e.eq_l = 0;
+    e.eq_m = 0;
+    e.eq_h = 0;
+    e.chorus_level = 0;
+    e.chorus_lfo_freq = 0.5f;
+    e.chorus_depth = 0.5f;
+    amy_add_event(&e);
+
+    print_events_for_patch_number(patch_number);    
+    
+    
     // Now just spin for a while
     uint32_t start = amy_sysclock();
     while(amy_sysclock() - start < 5000) {
